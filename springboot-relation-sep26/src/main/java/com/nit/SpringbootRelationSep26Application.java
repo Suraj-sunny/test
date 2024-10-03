d0654d6671d8e72bb81d4f661f47b1864fde313d
suraj <suraj@DESKTOP-24828V0>
2024-10-03 12:30:34 +0530

    commit with appilication

---
+++ springboot-relation-sep26/src/main/java/com/nit/SpringbootRelationSep26Application.java
@@ -0,0 +1,13 @@
+package com.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRelationSep26Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRelationSep26Application.class, args);
+	}
+
+}
