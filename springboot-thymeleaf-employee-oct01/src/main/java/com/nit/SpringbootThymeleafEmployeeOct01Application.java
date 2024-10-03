038bbde1c1ef376304bf7a899e6992d52e0d2629
suraj <suraj@DESKTOP-24828V0>
2024-10-03 11:24:27 +0530

    commit

---
+++ springboot-thymeleaf-employee-oct01/src/main/java/com/nit/SpringbootThymeleafEmployeeOct01Application.java
@@ -0,0 +1,13 @@
+package com.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootThymeleafEmployeeOct01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootThymeleafEmployeeOct01Application.class, args);
+	}
+
+}
